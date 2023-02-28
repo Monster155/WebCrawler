3748f8ce412119b10ba9e07fba26eee0d18765c9
damir <damir180501@mail.ru>
2023-02-28 15:56:55 +0300

    + Made bool search better

--- bool_search.py
+++ bool_search.py
@@ -11,21 +11,44 @@ indexes = {}
 with open(inverted_indexes_file, 'r', encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines:
-        arr = line.split(' ')
-        indexes[arr[0]] = [int(k) for k in arr[1:]]
+        pages_arr = line.split(' ')
+        indexes[pages_arr[0]] = [int(k) for k in pages_arr[1:]]
 
 search_request = input().split(' ')
 
+doc = Doc(search_request[0] + " " + search_request[2])
+doc.segment(segmenter)
+doc.tag_morph(morph_tagger)
+doc.tokens[0].lemmatize(morph_vocab)
+doc.tokens[1].lemmatize(morph_vocab)
+search_request[0] = doc.tokens[0].lemma.lower()
+search_request[2] = doc.tokens[1].lemma.lower()
+
+pages_arr = []
 if search_request[0] in indexes.keys() and search_request[2] in indexes.keys():
     if search_request[1].lower() == 'not':
-        arr = [value for value in indexes[search_request[0]] if value not in indexes[search_request[2]]]
-        print(set(arr))
+        pages_arr = [value for value in indexes[search_request[0]] if value not in indexes[search_request[2]]]
     if search_request[1].lower() == 'and':
-        arr = [value for value in indexes[search_request[0]] if value in indexes[search_request[2]]]
-        print(set(arr))
+        pages_arr = [value for value in indexes[search_request[0]] if value in indexes[search_request[2]]]
     if search_request[1].lower() == 'or':
-        arr = indexes[search_request[0]] + indexes[search_request[2]]
-        print(set(arr))
+        pages_arr = indexes[search_request[0]] + indexes[search_request[2]]
+else:
+    if search_request[0] not in indexes.keys() and search_request[1].lower() == 'not':
+        pages_arr = []
+    else:
+        if search_request[0] in indexes.keys():
+            print("Pages only for \"" + search_request[0] + "\"")
+            pages_arr = indexes[search_request[0]]
+        elif search_request[2] in indexes.keys():
+            print("Pages only for \"" + search_request[2] + "\"")
+            pages_arr = indexes[search_request[2]]
+        else:
+            pages_arr = []
+
+if len(pages_arr) == 0:
+    print("Pages not found")
+else:
+    print(set(pages_arr))
 
 # () > not > and > or
 
@@ -41,19 +64,3 @@ if search_request[0] in indexes.keys() and search_request[2] in indexes.keys():
 #     search_request = search_request + [')'] * brackets_count
 # if brackets_count < 0:
 #     search_request = ['('] * -brackets_count + search_request
-
-
-# search_values = []
-#
-# doc = Doc(search_request)
-# doc.segment(segmenter)
-# doc.tag_morph(morph_tagger)
-# for token in doc.tokens:
-#     token.lemmatize(morph_vocab)
-#     search_values.append(token.lemma.lower())
-#
-# for value in search_values:
-#     if value in indexes.keys():
-#         print(indexes[value])
-#     else:
-#         print("Can't find pages with word", value)
---
+++ test.py
@@ -0,0 +1,5 @@
+x = {2, 3, 5}
+y = {1, 2, 3}
+print(eval("x.symmetric_difference(y)"))
+print(eval("x.union(y)"))
+print(eval("x.intersection(y)"))
